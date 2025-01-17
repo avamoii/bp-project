8f7b0d0c2848fe3422d876595c357aca80f78772
Mahdi Haeri <mahdihaerim@gmail.com>
2025-01-17 16:16:31 +0330

    Fix crash score screen bug

--- src/screen_score.c
+++ src/screen_score.c
@@ -18,6 +18,7 @@ int recordIndex = 0;
 ScoreRecord records[MAX_RECORDS];
 
 void readInfo(void) {
+    printf("Start reading from file\n");
     // باز کردن فایل باینری برای خواندن اطلاعات
     FILE *file = fopen("output.bin", "rb");
 
@@ -26,8 +27,10 @@ void readInfo(void) {
         return;
     }
 
+    printf("file opened successfully\n");
 
-    while (!feof(file) && recordIndex < MAX_RECORDS) {
+    while (recordIndex < MAX_RECORDS) {
+        printf("Reading record %d\n", recordIndex);
         size_t nameLength, timeLength;
         char name[50], timeString[64];
         int score;
@@ -40,8 +43,11 @@ void readInfo(void) {
         if (fread(&score, sizeof(int), 1, file) != 1) break;
 
         // خواندن زمان
-        if (fread(&timeLength, 20, 1, file) != 1) break;
+        if (fread(&timeLength, sizeof(size_t), 1, file) != 1) break;
         if (fread(timeString, sizeof(char), timeLength, file) != timeLength) break;
+
+        name[nameLength] = '\0';
+        timeString[timeLength] = '\0';
 
         strcpy(records[recordIndex].name, name);
         strcpy(records[recordIndex].timeString, timeString);
@@ -56,6 +62,7 @@ void readInfo(void) {
 
 void InitScoreScreen(void) {
     recordIndex = 0;
+    IsScoreScreenFinished = false;
     readInfo();
 }
 
@@ -66,17 +73,18 @@ void UpdateScoreScreen(void) {
 }
 
 void DrawScoreScreen(void) {
-    ClearBackground(RAYWHITE);
+    ClearBackground(BLACK);
 
     for (int i = 0; i < recordIndex; i++) {
         char recordText[128];
         snprintf(recordText, sizeof(recordText), "Score : %d, Date and Time: %s", records[i].score, records[i].timeString);
-        DrawText(recordText, 50, 120, 20, YELLOW);
-        DrawText(records[i].name, 50, 400, 20, YELLOW);
+        DrawText(recordText, 50, 120 + i * 40, 20, YELLOW);
+        DrawText(records[i].name, 50, 400 + i * 40, 20, YELLOW);
     }
 }
 
 void UnloadScoreScreen(void) {
+
 }
 
 bool FinishScoreScreen(void) {
