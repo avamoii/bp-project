a439b795b600e1992e70fd50de757e8c9d0ac4a3
Mahdi Haeri <mahdihaerim@gmail.com>
2025-01-18 17:01:03 +0330

    Clean main.c

--- src/main.c
+++ src/main.c
@@ -12,7 +12,6 @@ int screenHeight = 720;
 //----------------------------------------------------------------------------------
 // Module Variables Definition (local)
 //----------------------------------------------------------------------------------
-
 static float transAlpha = 0;
 static bool onTransition = false;
 static bool transFadeOut = false;
@@ -23,19 +22,17 @@ static int framesCounter = 0;
 //----------------------------------------------------------------------------------
 // Module Functions Declaration (local)
 //----------------------------------------------------------------------------------
-static void ChangeToScreen(int screen);     // Change to screen, no transition effect
+static void ChangeToScreen(int screen); // Change to screen, no transition effect
 static void TransitionToScreen(int screen); // Request transition to next screen
-static void UpdateTransition(void);         // Update transition effect
-static void DrawTransition(void);           // Draw transition effect (full-screen rectangle)
-static void UpdateDrawFrame(void);          // Update and draw one frame
-
+static void UpdateTransition(void); // Update transition effect
+static void DrawTransition(void); // Draw transition effect (full-screen rectangle)
+static void UpdateDrawFrame(void); // Update and draw one frame
 
 
 //----------------------------------------------------------------------------------
 // Main entry point
 //----------------------------------------------------------------------------------
-int main()
-{
+int main() {
     InitWindow(screenWidth, screenHeight, "PacMan Game - Ava");
 
     // Set window to full screen mode
@@ -43,22 +40,16 @@ int main()
 
     SetTargetFPS(60);
 
-    // Load global data here (assets that must be available in all screens, i.e. fonts)
-
     // Define and init first screen
     // NOTE: currentScreen is defined in screens.h as a global variable
     currentScreen = MENU;
     InitMenuScreen();
-    // Texture2D background = LoadTexture("../tools/background4.jpg");
 
     // Main game loop
-    // Main game loop
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         UpdateDrawFrame();
     }
     return 0;
-
 }
 
 
@@ -66,60 +57,61 @@ int main()
 // Module Functions Definition (local)
 //----------------------------------------------------------------------------------
 // Change to screen, no transition effect
-static void ChangeToScreen(int screen)
-{
-    switch (currentScreen)
-    {
-    case MENU: UnloadMenuScreen(); break;
-    case NAME: UnloadnamePageScreen(); break;
-    case SCORE: UnloadScoreScreen(); break;
-    case ENDING: UnloadEndingScreen(); break;
-    case GAMEPLAY: UnloadGameplayScreen(); break;
-    default: break;
+static void ChangeToScreen(int screen) {
+    switch (currentScreen) {
+        case MENU: UnloadMenuScreen();
+            break;
+        case NAME: UnloadnamePageScreen();
+            break;
+        case SCORE: UnloadScoreScreen();
+            break;
+        case ENDING: UnloadEndingScreen();
+            break;
+        case GAMEPLAY: UnloadGameplayScreen();
+            break;
+        default: break;
     }
 
-    switch (screen)
-    {
-    case MENU: InitMenuScreen(); break;
-    case NAME: InitnamePageScreen(); break;
-    case SCORE: InitScoreScreen(); break;
-    case ENDING: InitEndingScreen(); break;
-    case GAMEPLAY: InitGameplayScreen(); break;
-    default: break;
+    switch (screen) {
+        case MENU: InitMenuScreen();
+            break;
+        case NAME: InitnamePageScreen();
+            break;
+        case SCORE: InitScoreScreen();
+            break;
+        case ENDING: InitEndingScreen();
+            break;
+        case GAMEPLAY: InitGameplayScreen();
+            break;
+        default: break;
     }
 
     currentScreen = screen;
 }
 
 // Request transition to next screen
-static void TransitionToScreen(int screen)
-{
+static void TransitionToScreen(int screen) {
     onTransition = true;
     transFromScreen = currentScreen;
     transToScreen = screen;
 }
 
 // Update transition effect
-static void UpdateTransition(void)
-{
-    if (!transFadeOut)
-    {
+static void UpdateTransition(void) {
+    if (!transFadeOut) {
         transAlpha += 0.05f;
 
-        if (transAlpha >= 1.0)
-        {
+        if (transAlpha >= 1.0) {
             transAlpha = 1.0;
             currentScreen = transToScreen;
             transFadeOut = true;
             framesCounter = 0;
         }
-    }
-    else  // Transition fade out logic
+    } else // Transition fade out logic
     {
         transAlpha -= 0.05f;
 
-        if (transAlpha <= 0)
-        {
+        if (transAlpha <= 0) {
             transAlpha = 0;
             transFadeOut = false;
             onTransition = false;
@@ -130,46 +122,41 @@ static void UpdateTransition(void)
 }
 
 // Draw transition effect (full-screen rectangle)
-static void DrawTransition(void)
-{
+static void DrawTransition(void) {
     DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, transAlpha));
 }
 
 // Update and draw one frame
-void UpdateDrawFrame(void)
-{
-
+void UpdateDrawFrame(void) {
     // Update
     //----------------------------------------------------------------------------------
-    if (!onTransition)
-    {
+    if (!onTransition) {
         switch (currentScreen) {
-        case MENU:
-            UpdateMenuScreen();
-            int nextScreen = FinishMenuScreen();
-            if (nextScreen != MENU) ChangeToScreen(nextScreen);
-            break;
-        case NAME:
-            UpdatenamePageScreen();
-            if (FinishnamePageScreen()) ChangeToScreen(GAMEPLAY);
-            break;
-        case GAMEPLAY:
-            UpdateGameplayScreen();
-            if (FinishGameplayScreen()) ChangeToScreen(ENDING);
-            break;
-        case SCORE:
-            UpdateScoreScreen();
-            if (FinishScoreScreen()) ChangeToScreen(MENU);
-            break;
-        case ENDING:
-            UpdateEndingScreen();
-            if (FinishEndingScreen()) ChangeToScreen(MENU);
-            break;
-        default:
-            break;
+            case MENU:
+                UpdateMenuScreen();
+                int nextScreen = FinishMenuScreen();
+                if (nextScreen != MENU) ChangeToScreen(nextScreen);
+                break;
+            case NAME:
+                UpdatenamePageScreen();
+                if (FinishnamePageScreen()) ChangeToScreen(GAMEPLAY);
+                break;
+            case GAMEPLAY:
+                UpdateGameplayScreen();
+                if (FinishGameplayScreen()) ChangeToScreen(ENDING);
+                break;
+            case SCORE:
+                UpdateScoreScreen();
+                if (FinishScoreScreen()) ChangeToScreen(MENU);
+                break;
+            case ENDING:
+                UpdateEndingScreen();
+                if (FinishEndingScreen()) ChangeToScreen(MENU);
+                break;
+            default:
+                break;
         }
-    }
-    else UpdateTransition();
+    } else UpdateTransition();
 
     // UpdateMusicStream(music);
     //----------------------------------------------------------------------------------
@@ -179,16 +166,18 @@ void UpdateDrawFrame(void)
     BeginDrawing();
     ClearBackground(WHITE);
     switch (currentScreen) {
-    case MENU: DrawMenuScreen(); break;
-    case NAME: DrawnamePageScreen(); break;
-    case GAMEPLAY: DrawGameplayScreen(); break;
-    case SCORE: DrawScoreScreen(); break;
-    case ENDING: DrawEndingScreen(); break;
-    default: break;
+        case MENU: DrawMenuScreen();
+            break;
+        case NAME: DrawnamePageScreen();
+            break;
+        case GAMEPLAY: DrawGameplayScreen();
+            break;
+        case SCORE: DrawScoreScreen();
+            break;
+        case ENDING: DrawEndingScreen();
+            break;
+        default: break;
     }
-        if (onTransition) DrawTransition();
+    if (onTransition) DrawTransition();
     EndDrawing();
-    //----------------------------------------------------------------------------------
 }
-
-
